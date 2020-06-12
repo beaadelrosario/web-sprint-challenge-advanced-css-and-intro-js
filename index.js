35acdd15d349859bbabe48911b79aab1d82c7bd6
Bea DelRosario <bdelronyc@Beas-MacBook-Pro.local>
2020-06-12 10:40:16 -0700

    struggling with keys as strings

--- index.js
+++ index.js
@@ -200,6 +200,7 @@ const artists = [
       "paintings": 328
     }
 ]
+console.log ('test');
 
 // 🖌🖼 M V P 🖼🖌 //
 
@@ -208,24 +209,26 @@ const artists = [
 (1) Name of the first artist (0th index) in the array
 (2) Bio of the third artist (2nd index) in the array */
 
+console.log (artists[0]); /*part 1 answer*/
 
+console.log(artists[2]);
 
 /* Task 2: There is a typo in your dataset 😱 The 9th artist, Vincent Van Gogh is currently Vincent Van Dough. Use an array method to fix this issue and console.log() to check your work. */
 
-
+artists.splice
 
 /* Task 3: Create a function called `getArtistByIndex` that takes two arguments:
  *     (1) artists array
  *     (2) a number which is the desired index in the array.
  * getArtistByIndex returns a string in the format `The artist at index {id} is {name}.`
  * 
- * For example, if getArtistByIndex is invoked with the artists dataset and the number 0,
+ * For example, if getArtistByIndex is invoked with the artists dataset and the number 0, (artists,0)
  * it will return `The artist at index 0 is Amedeo Modigliani`.
 */
 function getArtistByIndex(array, index) {
-    /* code here */
+    return `The artist at index ${this.id} is ${this.name}.`
   }
-  
+  console.log (getArtistByIndex(artists,0));
   /**
 
 
